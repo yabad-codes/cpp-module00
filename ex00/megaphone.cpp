7db4e89ba4ec85c94e7343471cbb6f0759786479
Yahya ABAD <yahya.abad9@gmail.com>
2023-07-06 09:21:02 +0100

    doing it c++ way

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,31 +6,16 @@
 /*   By: yabad <yabad@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 14:57:48 by yabad             #+#    #+#             */
-/*   Updated: 2023/07/05 15:06:39 by yabad            ###   ########.fr       */
+/*   Updated: 2023/07/06 09:18:26 by yabad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-void	print_arg(char *arg)
-{
-	int	i = 0;
-
-	while (arg[i])
-	{
-		std::cout << (char)toupper(arg[i]);
-		i++;
-	}
-}
-
-void	print_args(char **av)
-{
-	int	i = 1;
-
-	while (av[i])
-	{
-		print_arg(av[i]);
-		i++;
+void	upperStr(std::string str) {
+	int	len = str.size();
+	for (int i = 0; i < len; i++) {
+		std::cout << (char)toupper(str[i]);
 	}
 }
 
@@ -38,8 +23,11 @@ int	main(int ac, char **av)
 {
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else
-		print_args(av);
+	else {
+		for (int i = 1; i < ac; i++) {
+			upperStr(av[i]);
+		}
+	}
 	std::cout << std::endl;
 	return (0);
 }
